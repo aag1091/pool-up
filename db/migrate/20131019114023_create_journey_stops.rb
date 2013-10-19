770a377a80f77d677303350211f74d038c2030b6
prakash sejwani <prakashsejwani@gmail.com>
2013-10-19 19:43:10 +0530

    rename spot with stop :)

--- db/migrate/20131019114023_create_journey_stops.rb
+++ db/migrate/20131019114023_create_journey_stops.rb
@@ -1,6 +1,6 @@
 class CreateJourneyStops < ActiveRecord::Migration
   def change
-    create_table :journey_spots do |t|
+    create_table :journey_stops do |t|
       t.references :journey, index: true
       t.references :stop, index: true
       t.string :stop_type
