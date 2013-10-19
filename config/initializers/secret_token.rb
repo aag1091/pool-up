abe54c91ac0d829cc65bbbe0e08c7442d7c8ac7b
Avinash Gosavi <aag1091@gmail.com>
2013-10-19 06:38:18 +0530

    added rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PoolUp::Application.config.secret_key_base = 'e60eb35768cea10d172e2294a69787231e55e6ebf8f6af98a928c9f0e675631130920898c93b0c3e212e04b7dc5c49604ca29177ffb03dfc2f2fc7a9e8ac9a0c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PoolUp::Application.config.session_store :cookie_store, key: '_pool-up_session'
