155670ac581e73f6417a82be127ab53894efa1d1
prakash sejwani <prakashsejwani@gmail.com>
2013-10-21 00:52:27 +0530

    added mumbai map image in home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 PoolUp::Application.routes.draw do
-  
   resources :vehicles
   resources :driver_journeys
   resources :passenger_journeys
@@ -8,6 +7,7 @@ PoolUp::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   get "home/index"
   devise_for :users
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
